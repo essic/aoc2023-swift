bbedda1733afbe2ccb1e96f198ecd505f44d71e9
gangineering <lollancf37@gmail.com>
2024-11-07 02:08:54 +0100

    day1: final implementation (at the time...)

--- Sources/aoc2023-swift/aoc2023.swift
+++ Sources/aoc2023-swift/aoc2023.swift
@@ -4,64 +4,56 @@
 import Algorithms
 import Foundation
 
-let alsoValidDigits = [
+let fromWordToDigitStr = [
     "one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7",
     "eight": "8", "nine": "9",
 ]
 
-func findAllOccurences(of str: String, with target: String) -> (digit: String, positions: [Int]) {
+let fromDigitStrToDigit = Dictionary.init(
+    uniqueKeysWithValues: fromWordToDigitStr.values.map { ($0, Int($0)!) }
+)
+
+let allDigits = chain(fromWordToDigitStr.keys, fromWordToDigitStr.values)
+
+func findAllOccurences(of str: String, with target: String) -> (occurence: String, positions: [Int])
+{
     var startSearchFrom = str.startIndex
-    var results = [Int]()
+    var occurencePositions = [Int]()
     while startSearchFrom < str.endIndex {
-        guard let result = str[startSearchFrom...].range(of: target) else {
+        guard let occurenceRange = str[startSearchFrom...].range(of: target) else {
             break
         }
 
-        let indexOfOccurence = str.distance(from: str.startIndex, to: result.lowerBound)
-        results.append(indexOfOccurence)
-        startSearchFrom = result.upperBound
+        let indexOfOccurence = str.distance(from: str.startIndex, to: occurenceRange.lowerBound)
+        occurencePositions.append(indexOfOccurence)
+        startSearchFrom = occurenceRange.upperBound
     }
-    return (target, results)
+    return (target, occurencePositions)
 }
 
-func findAllDigitsOccurences(of str: String) -> [String] {
-    let allDigits = chain(alsoValidDigits.keys, alsoValidDigits.values)
-    let result =
-        allDigits.flatMap {
-            let localOccurences = findAllOccurences(of: str, with: $0)
-            return localOccurences.positions.map {
-                return ($0, localOccurences.digit)
-            }
-        }.sorted {
-            $0.0 < $1.0
-        }.map { $0.1 }
-    return result
+//Why mainActor ?
+@MainActor func allDigitOccurencesSortedByAppearingPosition(of str: String) -> [String] {
+    return allDigits.flatMap {
+        let localOccurences = findAllOccurences(of: str, with: $0)
+        return localOccurences.positions.map {
+            return ($0, localOccurences.occurence)
+        }
+    }.sorted {
+        $0.0 < $1.0
+    }.map { $0.1 }
 }
 
-do {
-    let data = try String(contentsOfFile: "day1-input.txt")
-    var numbers = [Int]()
-    data.enumerateLines { line, stop -> Void in
-        let result = findAllDigitsOccurences(of: line)
-        let digits = result.map {
-            if let value = alsoValidDigits[$0] {
-                return Int(value)!
-            } else {
-                return Int($0)!
-            }
-        }   
-
-        if digits.count > 1 {
-            numbers.append(10 * digits.first! + digits.last!)
+let data = try String(contentsOfFile: "day1-input.txt")
+var total = 0
+data.enumerateLines { line, _ -> Void in
+    let digits = allDigitOccurencesSortedByAppearingPosition(of: line).map {
+        if let digitStr = fromWordToDigitStr[$0] {
+            return fromDigitStrToDigit[digitStr]!
         } else {
-            numbers.append(10 * digits.first! + digits.first!)
+            return fromDigitStrToDigit[$0]!
         }
     }
-
-    let sum = numbers.reduce(0) { x, y in
-        x + y
-    }
-    print(sum)
-} catch {
-    print("Could not find file.")
+    total += 10 * digits.first! + (digits.count > 1 ? digits.last! : digits.first!)
 }
+
+print(total)
